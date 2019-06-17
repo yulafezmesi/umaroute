cafb3eda6509494228394f506daf0461c0bb4186
sirket pc <mcertel@ugurgrp.local>
2019-06-17 16:41:35 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,80 @@
 <template>
   <div id="app">
+    <div id="map"></div>
+    <button id="submit"></button>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const directionsService = new google.maps.DirectionsService();
+const directionsDisplay = new google.maps.DirectionsRenderer();
+import HelloWorld from "./components/HelloWorld.vue";
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      map: "",
+      fetcedData: ["selam"],
+      location: "",
+      isSend: false,
+      isDone: false,
+      formValues: {
+        waypts: [],
+        realDistance: 0,
+        totalDistance: 0,
+        vehicleType: "",
+        vehicleValue: 0,
+        endPoint: "",
+        pointCount: 0,
+        beginPoint: "Nazilli, Aydın, Türkiye",
+        extraCharge: 0,
+        description: "",
+        shippingNumber: ""
+      }
+    };
+  },
+  computed: {},
   components: {
     HelloWorld
+  },
+  methods: {
+    initMap() {
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: { lat: 39.01, lng: 30.97 }
+      });
+      // directionsDisplay.setMap(this.map);
+      document.getElementById("submit").addEventListener("click", function() {
+        if (this.formValues.endPoint) {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        } else {
+          alert("bitiş noktası belirtmeniz gerekiyor");
+        }
+      });
+      google.maps.Polyline.prototype.getBounds = function(startBounds) {
+        if (startBounds) {
+          var bounds = startBounds;
+        } else {
+          var bounds = new google.maps.LatLngBounds();
+        }
+
+        this.getPath().forEach(function(item, index) {
+          bounds.extend(new google.maps.LatLng(item.lat(), item.lng()));
+        });
+        return bounds;
+      };
+    }
+  },
+  mounted() {
+    this.initMap();
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 Vue.config.productionTip = false
+
+// export const loadedGoogleMapsAPI = new Promise( (resolve,reject) => {
+
+//   window['GoogleMapsInit'] = resolve;
+
+//   let GMap = document.createElement('script');
+  
+//   GMap.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=AIzaSyAjEn4M4V78EDEku1FoJ1p7AdGHSjHzp3c&callback=GoogleMapsInit&libraries=places`);
+
+//   document.body.appendChild(GMap); 
+// });
+
+
 
 new Vue({
   render: h => h(App),
